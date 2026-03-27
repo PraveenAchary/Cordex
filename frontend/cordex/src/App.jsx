bf01fc23b543efb5af424cd0425ca094ba02e199
PraveenAchary <praveenvishwabramham@gmail.com>
2026-03-27 13:21:52 +0530

    Update App.jsx

--- frontend/cordex/src/App.jsx
+++ frontend/cordex/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/docs" element={<CordexDocs/>}/>
       </Routes>
-    </BrowserRoutes>
+    </BrowserRouter>
   );
 }
 
